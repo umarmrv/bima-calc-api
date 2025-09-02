15f6923dca3a89aa8e4ca0b6ad8604f75fae9209
Shohjahon <sh.murodovv@gmail.com>
2025-09-02 14:17:11 +0500

    created views

--- bima/views.py
+++ bima/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Quote, Application
+from .serializer import (
+    QuoteCreateSerializer, QuoteDetailSerializer,
+    ApplicationCreateSerializer, ApplicationDetailSerializer
+)
 
-# Create your views here.
+class QuoteViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        return Quote.objects.filter(user=self.request.user).order_by("-created_at")
+    def get_serializer_class(self):
+        return QuoteCreateSerializer if self.action == "create" else QuoteDetailSerializer
+
+class ApplicationViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        return Application.objects.filter(user=self.request.user).order_by("-created_at")
+    def get_serializer_class(self):
+        return ApplicationCreateSerializer if self.action == "create" else ApplicationDetailSerializer
