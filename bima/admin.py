4f47d265d522eb65b7bc347fadf750cd108560a7
Shohjahon <sh.murodovv@gmail.com>
2025-09-02 14:26:16 +0500

    registered models in admin.py

--- bima/admin.py
+++ bima/admin.py
@@ -1,3 +1,13 @@
+# core/admin.py
 from django.contrib import admin
+from .models import Quote, Application
 
-# Register your models here.
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ("id","user","tariff","total_amount","status","valid_until","created_at")
+    list_filter = ("tariff","status","created_at")
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ("id","user","tariff","total_amount_snapshot","status","created_at")
+    list_filter = ("tariff","status","created_at")
